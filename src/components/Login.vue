3d58333a71522fb7261e9f651511ee88085ee9fb
Pedro Guerrero <pguerrerox@gmail.com>
2025-02-28 16:32:26 -0500

    0.1.9 - changelog

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,10 +7,32 @@ const projectStore = useProjectStore()
 const projectManagers = computed(() => projectStore.roProjectManagers)
 const selectedManager = ref(null)
 
-function handleSubmit() {
-    projectStore.setActiveManager(selectedManager)
-    if (selectedManager.value) {
-        router.push('/')
+
+async function handleLogin() {
+    console.log(selectedManager.value);
+    
+    try {
+        const response = await fetch('http://localhost:5000/auth/login', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify({managerName: selectedManager.value})
+        })
+
+        if (!response.ok) {
+            const error = await response.json()
+            throw new Error(error.message)
+        }
+        const data = await response.json()
+        console.log(`${selectedManager} loged in succesfully - ${data}`)
+       
+        projectStore.setActiveManager(selectedManager)
+        if (selectedManager.value) {
+            router.push('/')
+        }
+    }
+    catch (err) {
+        console.error(err)
     }
 }
 </script>
@@ -19,7 +41,7 @@ function handleSubmit() {
     <h1>Please Login</h1>
     <h4>Select your name from the list below</h4>
 
-    <form @submit.prevent="handleSubmit">
+    <form @submit.prevent="handleLogin">
         <label for="project_managers">Project Managers: </label>
         <select id="project_managers" name="project_managers" v-model="selectedManager">
             <option v-for="(manager, index) in projectManagers" :key="index" :value="manager">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,19 @@
 import { useProjectStore } from '../store/projectStore.js';
 import { RouterLink } from 'vue-router';
 
+import Header from '@/components/Header.vue';
+import Navigation from '@/components/Navigation.vue';
+
 const projectStore = useProjectStore();
 
 </script>
 
 <template>
+    <Header />
+    <Navigation />
     <h1>HOME</h1>
     <h3>Active Manager: {{ projectStore.activeManager }}</h3>
+    <RouterView/>
     <!-- <div v-for="(project, index) in projectStore.readOnly" :key="index" class="project-card">
         <RouterLink :to="`/projects/${project.project_number}`">
             <p>Project Number: {{ project.project_number }}</p>
